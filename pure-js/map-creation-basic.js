6f0170c3296085175505c13bfefaa54d7d234fb4
Eric Härtel <eric.haertel@posteo.de>
2023-04-11 12:55:57 +0200

    fix JSON parsing, wordpress integration

--- pure-js/map-creation-basic.js
+++ pure-js/map-creation-basic.js
@@ -24,15 +24,15 @@ L.tileLayer(
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    createMarker(map, [52.5051, 13.4334], 'Thema A', 'open' ,'kurze Beschreibung', 'https://leafletjs.com/reference.html#popup')
-    createMarker(map, [52.5151, 13.4334], 'Thema B', 'done' ,'hier steht auch irgendwas', 'https://leafletjs.com/reference.html#popup')
-    createMarker(map, [52.5251, 13.4334], 'Thema C', 'unknown' ,'und noch mehr Beschreibung mit viel BlahBlah und so weiter und so fort', 'https://leafletjs.com/reference.html#popup')
+createMarker(map, [52.5051, 13.4334], 'Thema A', 'open' ,'kurze Beschreibung', 'https://leafletjs.com/reference.html#popup')
+createMarker(map, [52.5151, 13.4334], 'Thema B', 'done' ,'hier steht auch irgendwas', 'https://leafletjs.com/reference.html#popup')
+createMarker(map, [52.5251, 13.4334], 'Thema C', 'unknown' ,'und noch mehr Beschreibung mit viel BlahBlah und so weiter und so fort', 'https://leafletjs.com/reference.html#popup')
 
-    fetch('geo.json')
-      .then(response => response.json())
-      .then(jsonObj => {
-        var polygon = L.geoJSON(jsonObj).addTo(map);
-        polygon.bindPopup("test");
-      })
-      .catch(error => console.error(error));
+fetch('geo.json')
+  .then(response => response.json())
+  .then(jsonObj => {
+    var polygon = L.geoJSON(jsonObj).addTo(map);
+    polygon.bindPopup("test");
+  })
+  .catch(error => console.error(error));
 
