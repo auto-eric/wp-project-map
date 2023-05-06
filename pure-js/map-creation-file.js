7d7b1fbc77776f13ee5181908ffeed18e0daea09
Eric Härtel <eric.haertel@posteo.de>
2023-05-06 17:55:25 +0200

    better HTML integration

--- pure-js/map-creation-file.js
+++ pure-js/map-creation-file.js
@@ -1,3 +1,72 @@
+const defaultStyle= {
+    "color": "#333333",
+    "weight": "2",
+    "opacity": 0.65
+};
+const bikeStyle= {
+    "color": "#3300bb",
+    "weight": "2",
+    "opacity": 0.65
+};
+const pedestrianStyle= {
+    "color": "#bb00bb",
+    "weight": "2",
+    "opacity": 0.65
+};
+
+var projectTypes = new Set();
+
+function handleClick(checkbox) {
+    console.log("change " + checkbox.value + " -> value: " + checkbox.checked)
+
+    
+}
+
+function handleProject(project) {
+    const geoJson = JSON.parse(project.geojson);
+    switch (project.type) {
+        case "bike":
+            var theStyle = bikeStyle;
+            break;
+        case "pedestrian":
+            var theStyle = pedestrianStyle;
+            break;
+        default:
+            var theStyle = defaultStyle;
+    }
+    var polygon = L.geoJSON(geoJson, {style: theStyle}).addTo(map);
+    polygon.bindPopup("<h3>"+project.title+"</h3><p>" + project.Hintergrund + "</p>");
+}
+
+async function loadProjects() {
+    try {
+        const response = await fetch('../data/projects.json');
+        const jsonObj = await response.json();
+        const types = jsonObj.data.map(project => {
+            handleProject(project);
+            return project.type;
+        });
+        const projectTypes = new Set(types);
+        console.log("types: " + projectTypes);
+        return projectTypes;
+    } catch (error) {
+        console.log('could not read data', error);
+    }
+}
+
+async function initialize() {
+    projectTypes = await loadProjects();
+
+    var checkboxHtml = "<form>";
+    projectTypes.forEach(t => {
+        checkboxHtml += "<input type=\"checkbox\" id=\"" + t + "\" value=\"" + t +"\" onclick=\"handleClick(this)\"> <label for=\"" + t + "\">" + t + "</label>"
+    });
+    checkboxHtml += "</form>";
+    console.log(checkboxHtml);
+
+    document.getElementById("filter").innerHTML = checkboxHtml;
+}
+
 var map = L.map('map-file', { center: [52.5051, 13.4334], zoom: 13 });
 
 L.tileLayer(
@@ -8,15 +77,4 @@ L.tileLayer(
     }).addTo(map);
 
 
-fetch('../data/projects.json')
-    .then(response => response.json())
-    .then(jsonObj => {
-        console.log("data: " + jsonObj.data)
-        jsonObj.data.forEach( project => {
-            const geoJson = JSON.parse(project.geojson);
-            console.log("geoJson: " + geoJson);
-            console.log("type", geoJson.type)
-            var polygon = L.geoJSON(geoJson).addTo(map);
-            polygon.bindPopup("<h1>"+project.title+"</h1><p>" + project.Hintergrund + "</p>");
-        });
-    })
+initialize();
